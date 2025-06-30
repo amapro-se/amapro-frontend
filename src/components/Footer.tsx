17ac2d15b59372202ba893515422ed8cf3edaee0
Choi YuJin <yjyj0774@gmail.com>
2025-06-30 17:25:22 +0900

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-full py-4 px-6 bg-zinc-100 text-zinc-700 text-sm text-center">
+      © 2025 AMAPRO. All rights reserved.
+    </footer>
+  );
+}
