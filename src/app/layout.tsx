9c8a4a8ceb40b0da036fb54efb0dbdccdd5080f3
Choi YuJin <yjyj0774@gmail.com>
2025-07-01 19:59:23 +0900

    move header component into components/Header/ directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { Header } from '@/components/Header';
+import { Header } from '@/components/Header/Header';
 import { Footer } from '@/components/Footer';
 import './globals.css';
 
