44c9b06f4dca1bc6e8950ec4523154b8272a90b1
Choi YuJin <yjyj0774@gmail.com>
2025-07-01 19:58:04 +0900

    add header user button component

---
+++ src/components/Header/buttons/HeaderUserButton.tsx
@@ -0,0 +1,9 @@
+export function HeaderUserButton() {
+  return (
+    <button
+      className="w-5 h-5 bg-zinc-100 absolute right-6"
+      onClick={() => console.log('mypage')}
+      title="user"
+    />
+  );
+}
