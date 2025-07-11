3ae81b59e26543deceffdc05fe6a6159cf0f875b
Choi YuJin <yjyj0774@gmail.com>
2025-07-11 22:55:06 +0900

    add main title

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -1,3 +1,10 @@
 export function ContestListPage() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-zinc-900 pb-4">
+        {/* 메인 타이틀 */}
+        <div className="text-white text-[40px] font-extrabold text-center">CONTEST LIST</div>
+      </div>
+    </>
+  );
 }
