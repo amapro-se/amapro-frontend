994302520983303bf5931cec9ddc9761996cc23f
Choi YuJin <yjyj0774@gmail.com>
2025-07-11 22:55:36 +0900

    add search bar

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -4,6 +4,10 @@ export function ContestListPage() {
       <div className="bg-zinc-900 pb-4">
         {/* 메인 타이틀 */}
         <div className="text-white text-[40px] font-extrabold text-center">CONTEST LIST</div>
+        {/* 검색창 */}
+        <div>
+          <div className="bg-zinc-400 w-5/6 h-8 mx-auto rounded-md my-4"></div>
+        </div>
       </div>
     </>
   );
