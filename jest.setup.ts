e325fe1fea08e54fc3e262fd8784f7188d50de58
Choi YuJin <yjyj0774@gmail.com>
2025-07-12 16:46:43 +0900

    setup jest

---
+++ jest.setup.ts
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    refresh: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+  usePathname: () => '/',
+}));
